3ed321cead16dc6c9947c598a620de63589fdd03
Aster <aster@vers.site>
2022-11-08 16:23:08 +0000

    🎀 Update readme tutorial

--- projects/pretty-print/tests/readme.rs
+++ projects/pretty-print/tests/readme.rs
@@ -1,32 +1,3 @@
-# Pretty Printer
-
-This crate defines a
-[Wadler-style](http://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf)
-pretty-printing API.
-
-Start with the static functions of [PrettyTree](enum.Doc.html).
-
-## Quick start
-
-Let's pretty-print simple sexps!  We want to pretty print sexps like
-
-```lisp
-(1 2 3)
-```
-or, if the line would be too long, like
-
-```lisp
-((1)
- (2 3)
- (4 5 6))
-```
-
-A _simple symbolic expression_ consists of a numeric _atom_ or a nested ordered _list_ of
-symbolic expression children.
-
-
-
-```rust
 use pretty_print::*;
 use SExp::*;
 enum SExp {
@@ -65,4 +36,3 @@ fn main() {
         list.to_pretty(5)
     );
 }
-```
