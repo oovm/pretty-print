4d01a973d91b254a8d7e935e318629aeeefe5edd
Aster <aster@vers.site>
2023-06-15 21:39:12 +0800

    Define the pretty print kind

--- projects/pretty-print/src/helpers/mod.rs
+++ projects/pretty-print/src/helpers/mod.rs
@@ -11,3 +11,42 @@ mod soft_block;
 // mod affixes;
 
 pub use self::{hard_block::HardBlock, k_and_r_bracket::KAndRBracket, sequence::PrettySequence, soft_block::SoftBlock};
+
+/// Represents a pretty-printable tree provider.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PrettyPrintKind {
+    /// A normal text.
+    Normal,
+    /// A keyword.
+    Keyword,
+    /// A string.
+    String,
+    /// A number.
+    Number,
+    /// A macro.
+    Annotation,
+    /// An argument.
+    Argument,
+    /// A mutable argument.
+    ArgumentMutable,
+    /// A local variable.
+    Local,
+    /// A mutable local variable.
+    LocalMutable,
+    /// An operator.
+    Operator,
+    /// A structure.
+    Structure,
+    /// A class.
+    Class,
+    /// A union.
+    Union,
+    /// A disjoint union.
+    UnionDisjoint,
+    /// A variant.
+    Variant,
+    /// An interface.
+    Interface,
+    /// A trait.
+    Trait,
+}
